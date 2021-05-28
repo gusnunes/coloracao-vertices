cb196f63265cecc3317867f26a183f4f741ce0cc
gusnunes <daniloaugusto_nunes@hotmail.com>
2021-05-27 22:26:52 -0300

    escreve resultado da coloração em arquivo

--- coloracao_vertices.py
+++ coloracao_vertices.py
@@ -1,8 +1,21 @@
 import igraph as ig
 
 def plota_grafo(G):
+    # cores que serao usadas no grafo
+    palette = ig.RainbowPalette(n=qtd_vertices)
+    cores = [palette.get(i) for i in range(qtd_vertices)]
+
     layout = G.layout("kk")
     ig.plot(G, layout=layout, bbox=(800, 800))
+
+# arquivo contém a cor que cada vértice recebeu
+def escreve_arquivo(G):
+    file = open("resultado.txt", 'w')
+   
+    for vertice in G.vs:
+        file.write(f'{vertice["color"]}\n')
+    
+    file.close()
 
 def define_cor(cores, cores_vizinhos):
     for opcao in cores:
@@ -22,17 +35,16 @@ def main():
     file = "petersen.txt"
     G = ig.Graph.Read_Adjacency(file).as_undirected()
     
+    # lista de cores (representadas por inteiros)
     qtd_vertices = G.vcount()
+    cores = [i for i in range(qtd_vertices)]
 
-    # cores que serao usadas no grafo
-    palette = ig.RainbowPalette(n=qtd_vertices)
-    cores = [palette.get(i) for i in range(qtd_vertices)]
-    
     # primeiramente os vértices não tem cores
     G.vs["color"] = None
 
     coloracao_vertices(G,cores)
+    escreve_arquivo(G)
 
-    plota_grafo(G)
+    #plota_grafo(G)
 
 main()
